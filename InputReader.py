fc4d2fa166f6968440aaf3c2fdd702b74e3054f7
cs2085s <christoph.schirmel@alumni.fh-aachen.de>
2019-05-15 18:42:42 +0200

    null-null geht alleees easy It IS WeDneSDaY MY DuDEs

--- InputReader.py
+++ InputReader.py
@@ -29,12 +29,12 @@ class InputReader:
                     splitted = l.strip().split(" ")
                     if splitted[0] in laendermap.keys():
                         raise RuntimeError("Error. Datei '" + self.mFilename + "' enthaelt das gleiche Land doppelt. Das ist nicht erlaubt!\n")
-                    if int(splitted[1]) < 0:
-                        raise RuntimeError("Error. So schoen Anti-Materie auch ist, leider sind keine negativen Kennzahlen erlaubt.")
+                    if int(splitted[1]) < 1:
+                        raise RuntimeError("Error. So schoen Anti-Materie auch ist, leider sind keine negativen Kennzahlen erlaubt. Ausserdem muessen Kennzahlen mindestens den Wert 1 haben.")
                     for l in laender:
                         if l.xPos == splitted[2] and l.yPos == splitted[3]:
                             raise RuntimeError("Error. Die Datei enthaelt Laender mit der gleichen Position. Vatikanstadt oder was?!\n")
-                    neuland = Land(splitted[2], splitted[3], splitted[0], splitted[1])
+                    neuland = Land(float(splitted[2])+181, float(splitted[3])+181, splitted[0], splitted[1])
                     laender.append(neuland)
                     laendermap[splitted[0]] = neuland
                     nachbarmap[neuland] = []
